6027e843d1eaa3a5c662d06123b6b1ea497b76fd
jill1234 <jilllinhan@gmail.com>
2017-03-09 16:01:33 +0800

    test 02

--- topmeperp_v1/Controllers/UserManageController.cs
+++ topmeperp_v1/Controllers/UserManageController.cs
@@ -15,7 +15,7 @@ namespace topmeperp.Controllers
         // GET: UserManage
         public ActionResult Index()
         {
-            log.Info("index!!!");
+            log.Info("index!");
             return View();
         }
         [HttpPost]
