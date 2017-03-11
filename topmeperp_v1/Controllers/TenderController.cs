202d0176f8e5c4715b7ae8b64a279e97c074e834
jill1234 <jilllinhan@gmail.com>
2017-03-11 19:56:27 +0800

    3/10 更新

--- topmeperp_v1/Controllers/TenderController.cs
+++ topmeperp_v1/Controllers/TenderController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using topmeperp.Models;
+using topmeperp.Service;
+using System.IO;
 
 namespace topmeperp.Controllers
 {
@@ -29,25 +31,93 @@ namespace topmeperp.Controllers
         //POST:Create
         public ActionResult Create()
         {
+            logger.Info("new project page!!");
             return View();
         }
-    public ActionResult Edit()
+        [HttpPost]
+        public ActionResult Create(TND_PROJECT prj)
         {
+            logger.Info("create project process! project name=" + prj.PROJECT_NAME + ",ProjectId=" + prj.PROJECT_ID);
+            TnderProject service = new TnderProject();
+            service.newProject(prj);
+            return View("~/Views/Tender/Index.cshtml");
+        }
+        //POST:TaskAssign
+        public ActionResult Task()
+        {
+            logger.Info("task assign page!!");
+            return View();
+        }
+        public ActionResult RFQ()
+        {
+            logger.Info("inquiry page!!");
             return View();
         }
 
+        [HttpGet]
+        public ActionResult Details(string id)
+        {
+            logger.Info("project detail page pojectid = " + id);
+            TnderProject service = new TnderProject();
+            TND_PROJECT p = service.getProjectById(id);
+            return View(p);
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Upload()
+        {
+            logger.Info("upload!");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file, FormCollection form)
+        {
+            if (file != null)
+            {
+                logger.Info("index for Upload File:" + file.FileName);
+                logger.Info("get projectId=" + form.Get("projectId"));
+                logger.Info("get startrow=" + form.Get("startrow"));
+
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/UploadFile/TEST"), fileName);
+                    file.SaveAs(path);
+                    //Sample Code : 解析Excel 檔案
+                    ProjectItemFromExcel poiservice = new ProjectItemFromExcel();
+                    poiservice.InitializeWorkbook(path);
+                    poiservice.ConvertDataForTenderProject(form.Get("projectId"), int.Parse(form.Get("startrow")));
+                    logger.Info("convert finish:" + poiservice.lstProjectItem.Count);
+                    ViewBag.result = "共" + poiservice.lstProjectItem.Count + "筆資料<br/>" + poiservice.errorMessage;
+                }
+                return View("~/Views/Tender/Upload.cshtml");
+            }
+            else { return View(); }
+        }
         private List<topmeperp.Models.TND_PROJECT> SearchProjectByName(string projectname)
         {
-            logger.Info("search project by 名稱 =" + projectname);
-            List<topmeperp.Models.TND_PROJECT> lstProject = new List<TND_PROJECT>();
-            using (var context = new topmepEntities())
+            if (projectname != null)
             {
-                lstProject = context.TND_PROJECT.SqlQuery("select * from TND_PROJECT p "
-                    + "where p.PROJECT_NAME Like '%' + @projectname + '%';",
-                     new SqlParameter("projectname", projectname)).ToList();
+                logger.Info("search project by 名稱 =" + projectname);
+                List<topmeperp.Models.TND_PROJECT> lstProject = new List<TND_PROJECT>();
+                using (var context = new topmepEntities())
+                {
+                    lstProject = context.TND_PROJECT.SqlQuery("select * from TND_PROJECT p "
+                        + "where p.PROJECT_NAME Like '%' + @projectname + '%';",
+                         new SqlParameter("projectname", projectname)).ToList();
+                }
+                logger.Info("get project count=" + lstProject.Count);
+                return lstProject;
             }
-            logger.Info("get project count=" + lstProject.Count);
-            return lstProject;
+            else
+            {
+                return null;
+            }
         }
+
     }
 }
+
