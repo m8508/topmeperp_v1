897ee8cf5fb220b9fa11d416bed9626ccac4e23c
chu <chu@DESKTOP-759PC56>
2017-03-17 19:01:37 +0800

    權限設定

---
+++ topmeperp_v1/Controllers/RoleManageController.cs
@@ -0,0 +1,36 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using topmeperp.Service;
+
+namespace topmeperp.Controllers
+{
+    public class RoleManageController : Controller
+    {
+        ILog log = log4net.LogManager.GetLogger(typeof(UserManageController));
+        UserManage userService = new UserManage();
+        // GET: RoleManage
+        public ActionResult Index()
+        {
+            log.Info("index");
+            userService.getAllRole();
+            SelectList roles = new SelectList(userService.userManageModels.sysRole, "ROLE_ID", "ROLE_NAME");
+            ViewBag.roles = roles;
+            return View();
+        }
+        public ActionResult FunctionList()
+        {
+            string roleid = Request["roles"];
+            log.Debug(Request.IsAjaxRequest());
+            log.Info("index roleid=" + roleid);
+            return View(userService.getFunctions(roleid));
+        }
+        public void UpdatePrivilege()
+        {
+            log.Info("new privilege:" + Request["hadPrivilege"]);
+        }
+    }
+}
