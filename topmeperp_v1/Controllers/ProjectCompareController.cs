9382cef2044acc17bb0aa7334bde66d839b7cbfd
chu <chu@DESKTOP-759PC56>
2017-05-18 23:57:44 +0800

    增加

---
+++ topmeperp_v1/Controllers/ProjectCompareController.cs
@@ -0,0 +1,48 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using topmeperp.Models;
+using topmeperp.Service;
+
+namespace topmeperp.Controllers
+{
+    public class ProjectCompareController : Controller
+    {
+        RptCompareProjectPrice service = new RptCompareProjectPrice();
+        static ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        // GET: ProjectCompare
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getCompareData(FormCollection f)
+        {
+            log.Info("Source ProijectID=" + f["srcprojectid"] + ",Target ProjectId=" + f["tarprojectid"] + "," + f["hasPrice"]);
+            bool hasPriec = false;
+            bool hasProject = false;
+            if (null != f["hasPrice"])
+            {
+                hasPriec = true;
+            }
+            if (null != f["hasProject"])
+            {
+                hasProject = true;
+            }
+             
+            List<ProjectCompareData> lst = service.RtpGetPriceFromExistProject(f["srcprojectid"], f["tarprojectid"], hasProject, hasPriec);
+            ViewBag.Result = "共取得" + lst.Count + "筆資料!!";
+            return PartialView("_CompareData", lst);
+        }
+        public void Update(FormCollection f)
+        {
+            string[] lstItem = f["chkItem"].Split(',');
+            for(int i=0; i<lstItem.Count(); i++)
+            {
+                log.Info("ITEM_INFO=" + lstItem[i]);
+            }
+        }
+    }
+}
---
+++ topmeperp_v1/Models/BOExtentionModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace topmeperp.Models
+{
+    public class ProjectCompareData
+    {
+        public string SOURCE_PROJECT_ID { get; set; }
+        public string SOURCE_SYSTEM_MAIN { get; set; }
+        public string SOURCE_SYSTEM_SUB { get; set; }
+        public string SOURCE_ITEM_ID { get; set; }
+        public string SOURCE_ITEM_DESC { get; set; }
+        public Nullable<decimal> SRC_UNIT_PRICE { get; set; }
+        public string TARGET_PROJECT_ID { get; set; }
+        public string TARGET_ITEM_ID { get; set; }
+        public string TARGET_ITEM_DESC { get; set; }
+        public string TARGET_SYSTEM_MAIN { get; set; }
+        public string TARGET_SYSTEM_SUB { get; set; }
+        public Nullable<long> EXCEL_ROW_ID { get; set; }
+    }
+}
