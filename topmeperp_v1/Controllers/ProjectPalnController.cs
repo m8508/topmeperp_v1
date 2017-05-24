f1707dda35dc8f67a204498fe8aa3212ca5c86f4
chu <chu@DESKTOP-759PC56>
2017-05-24 11:23:28 +0800

    專案管理套件與更新

---
+++ topmeperp_v1/Controllers/ProjectPalnController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace topmeperp.Controllers
+{
+    public class ProjectPalnController : Controller
+    {
+        // GET: ProjectPaln
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
