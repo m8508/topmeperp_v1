1ce88a3f1eff113d57b571f50fec61aa88b51b6b
chu <chu@DESKTOP-759PC56>
2017-06-22 15:11:01 +0800

    修正新增詢價單排序

--- topmeperp_v1/Service/PoiService.cs
+++ topmeperp_v1/Service/PoiService.cs
@@ -2052,7 +2052,8 @@ namespace topmeperp.Service
             catch (Exception ex)
             {
                 logger.Error("Datetime format error: " + ex.Message);
-                throw new Exception("日期格式有錯(YYYY/MM/DD");
+                form.DUEDATE = DateTime.Now;
+               // throw new Exception("日期格式有錯(YYYY/MM/DD");
             }
             //電子信箱:	admin@topmep
             logger.Debug(sheet.GetRow(6).Cells[0].ToString() + "," + sheet.GetRow(6).Cells[1]);
