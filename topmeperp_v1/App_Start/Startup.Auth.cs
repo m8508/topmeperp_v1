132c7f9b5af69ce441ced2752b637696807fc76a
m8508 <m8508.tw@yahoo.com.tw>
2018-01-18 15:40:20 +0800

    加入麵包屑條

--- topmeperp_v1/App_Start/Startup.Auth.cs
+++ topmeperp_v1/App_Start/Startup.Auth.cs
@@ -24,6 +24,6 @@ namespace topmeperp
     }
     public class AppInfo
     {
-        public static string Version="1.0.14";
+        public static string Version="1.0.15";
     }
 }
--- topmeperp_v1/Filter/TopmepFilter.cs
+++ topmeperp_v1/Filter/TopmepFilter.cs
@@ -5,12 +5,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using topmeperp.Models;
 
 namespace topmeperp.Filter
 {
     public class AuthFilter : ActionFilterAttribute
     {
-        ILog log = log4net.LogManager.GetLogger(typeof(AuthFilter));
+        static ILog log = log4net.LogManager.GetLogger(typeof(AuthFilter));
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             log.Info("request URL=" + context.HttpContext.Request.RawUrl);
@@ -26,8 +27,8 @@ namespace topmeperp.Filter
             else
             {
                 log.Info("session not exist!!");
-                if (context.HttpContext.Request.RawUrl != "/" 
-                    && context.HttpContext.Request.RawUrl !="/topmeperp/")
+                if (context.HttpContext.Request.RawUrl != "/"
+                    && context.HttpContext.Request.RawUrl != "/topmeperp/")
                 {
                     log.Info("forward to login page");
                     context.Result = new RedirectToRouteResult(
@@ -39,6 +40,25 @@ namespace topmeperp.Filter
                 }
             }
 
+        }
+        public static string getBreadcrumb(string url)
+        {
+            string breadcrumbHtml = "";
+            List<SYS_FUNCTION> lst = (List<SYS_FUNCTION>)HttpContext.Current.Session["functions"];
+            //使用list 物件查詢功能
+            SYS_FUNCTION curFunction = lst.Find(x => x.FUNCTION_URI.Contains(url));
+            log.Info("cur url=" + curFunction);
+
+            if (curFunction.ISMENU == "Y")
+            {
+                //string[] breadcrumb = url.Split('/');
+                // for (int i = 1; i < breadcrumb.Length; i++)
+                // { breadcrumbHtml = breadcrumbHtml + "<li class='breadcrumb-item'><a href='"+ curFunction.FUNCTION_URI + "'>"+ curFunction.FUNCTION_NAME+ "</a></li>";
+                breadcrumbHtml = breadcrumbHtml + "<li class='breadcrumb-item'><a href='#'>" + curFunction.MODULE_NAME + "</a></li>";
+                breadcrumbHtml = breadcrumbHtml + "<li class='breadcrumb-item'><a href='" + curFunction.FUNCTION_URI + "'>" + curFunction.FUNCTION_NAME + "</a></li>";
+                // }
+            }
+            return breadcrumbHtml;
         }
     }
 
