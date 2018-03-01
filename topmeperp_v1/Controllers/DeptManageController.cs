728df6ba7fd35db950dc79268741494baf98d7e5
m8508 <m8508.tw@yahoo.com.tw>
2018-03-01 21:49:21 +0800

    修正問題

--- topmeperp_v1/Controllers/DeptManageController.cs
+++ topmeperp_v1/Controllers/DeptManageController.cs
@@ -1,6 +1,7 @@
 ﻿using log4net;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -13,53 +14,48 @@ namespace topmeperp.Controllers
     public class DeptManageController : Controller
     {
         static ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        UserManage userService = new UserManage();
+        DepartmentManage service = new DepartmentManage();
         // GET: UserManage
         public ActionResult Index()
         {
             logger.Info("index");
+            //1.是公司名稱
+            SelectList dept = new SelectList(service.getDepartmentOrg(1), "DEP_ID", "DEPT_NAME");
+            ViewBag.d_parentId = dept;
+            ViewBag.TreeString = service.getDepartment4Tree(0);
             return View();
         }
         [HttpPost]
         public ActionResult Query(FormCollection form)
         {
-            logger.Info("criteria user_id=" + form.Get("userid") + ",username=" + form.Get("username") + ",tel=" + form.Get("tel") + ",roleid=" + form.Get("roles"));
-            return PartialView("UserList", userService.userManageModels);
+            logger.Info("criteria dpep_name=" + form.Get("userid"));
+            return PartialView("DepartmentList", service.Org_department);
             //return View(userService.userManageModels);
         }
         //新增或修改部門
-        public String addUser(FormCollection form)
+        public String addDepartment(FormCollection form)
         {
             logger.Info("form:" + form.Count);
             string msg = "";
             //懶得把Form綁SYS_USER 直接先把Form 值填滿
-            SYS_USER u = new SYS_USER();
-            u.USER_ID = form.Get("u_userid").Trim();
-            u.USER_NAME = form.Get("u_name").Trim();
-            u.PASSWORD = form.Get("u_password").Trim();
-            u.EMAIL = form.Get("u_email").Trim();
-            u.TEL = form.Get("u_tel").Trim();
-            u.TEL_EXT = form.Get("u_tel_ext").Trim();
-            u.MOBILE = form.Get("u_mobile").Trim();
-            u.ROLE_ID = form.Get("roles").Trim();
+            ENT_DEPARTMENT d = new ENT_DEPARTMENT();
+            d.DEPT_CODE = form.Get("d_deptCode").Trim();
+            d.DEPT_NAME = form.Get("d_deptName").Trim();
+            d.DESC = form.Get("d_desc").Trim();
+            if (null != form.Get("d_parentId") && "" != form.Get("d_parentId").Trim())
+            {
+                d.PARENT_ID = Convert.ToInt64(form.Get("d_parentId").Trim());
+            }
             SYS_USER loginUser = (SYS_USER)Session["user"];
-            u.CREATE_ID = loginUser.USER_ID;
-            u.CREATE_DATE = DateTime.Now;
-            int i = userService.addNewUser(u);
-            if (i == 0)
-            {
-                msg = userService.message;
-            }
-            else
-            {
-                msg = "帳號更新成功";
-            }
+            d.CREATE_ID = loginUser.USER_ID;
+            d.CREATE_DATE = DateTime.Now;
+            msg = "新增部門成功(" + service.addDepartment(d) + ")";
 
-            logger.Info("Request:user_ID=" + form["u_userid"]);
+            //logger.Info("Request:user_ID=" + form["u_userid"]);
             return msg;
         }
         //取得某一Dept 基本資料
-        public string getUser(string deptid)
+        public string getDept(string deptid)
         {
             logger.Info("get Dept id=" + deptid);
 
